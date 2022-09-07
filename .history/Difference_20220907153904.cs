b75df3f3b42d3b8a4275b7de821f78e3a7d589d3
Surendra Maurya <mauryasurendra92@gmail.com>
2022-09-07 15:44:54 +0530

    change did in addition.cs

--- .history/Difference_20220907153904.cs
+++ .history/Difference_20220907153904.cs
@@ -1,11 +1,11 @@
 using System;
 
-public class HelloWorld
+public class Difference
 {
     public static void Main(string[] args)
     {
-        int a = 10, b = 20;
-        Console.WriteLine("Sum: " + a + b);
+        int a = 20, b = 10;
+        Console.WriteLine("Diff: " + (a - b));
         Console.WriteLine("Hello  World");
     }
 }
