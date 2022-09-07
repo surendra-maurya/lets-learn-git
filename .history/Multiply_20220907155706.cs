9a9184e3183b8c4ab305a5d938165b7203c7f307
Surendra Maurya <mauryasurendra92@gmail.com>
2022-09-07 15:58:12 +0530

    new file multiply.cs added in new branch

---
+++ .history/Multiply_20220907155706.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Multiply
+{
+    public static void Main(string[] args)
+    {
+        int a = 20, b = 10;
+        Console.WriteLine("Multiply: " + (a * b));
+        Console.WriteLine("Hello  World");
+    }
+}
