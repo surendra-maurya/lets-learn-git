f4d4bb313de7c1146ffb1abf5cf31e8a923c77f6
Surendra Maurya <mauryasurendra92@gmail.com>
2022-09-07 16:20:56 +0530

    division.cs added

---
+++ Division.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Division
+{
+    public static void Main(string[] args)
+    {
+        int a = 20, b = 10;
+        Console.WriteLine("Div: " + (a / b));
+        Console.WriteLine("Hello  World");
+    }
+}
