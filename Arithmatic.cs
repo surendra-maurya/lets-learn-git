db4aaf86d4823a9ea2fbdbcd6f2c3d7f70e3c9d7
Surendra Maurya <mauryasurendra92@gmail.com>
2022-09-17 17:49:52 +0530

    new file added on 17/Sep/2022 17:49

---
+++ Arithmatic.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Difference
+{
+    public static void Main(string[] args)
+    {
+        int a = 20, b = 10;
+        Console.WriteLine("Diff: " + (a - b));
+        Console.WriteLine("Surendra");
+    }
+}
